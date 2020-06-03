b1b8d97343903014c52987d32e976d6a2ca6efeb
xphong <xphong@gmail.com>
2020-06-03 18:54:11 -0400

    Add router and theme

--- App.js
+++ App.js
@@ -1,40 +1,12 @@
 import * as React from 'react';
 import { Provider } from 'react-redux';
-import { Platform, StyleSheet, Text, View } from 'react-native';
 import store from './app/redux/store';
-
-const instructions = Platform.select({
-  ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
-  android: `Double tap R on your keyboard to reload,\nShake or press menu button for dev menu`,
-});
+import Router from './app/config/routes';
 
 export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native test!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <Router />
     </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
---
+++ app/config/routes.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+import { AppLoading } from 'expo';
+
+import { Scene, Router, Stack } from 'react-native-router-flux';
+
+export default class extends React.Component {
+  state = {
+    isReady: false
+  };
+
+  componentDidMount() {
+    setTimeout(()=> this.setState({isReady: true}), 1000)
+  }
+
+  render() {
+    let navTitleStyle = { fontSize: 15 , fontFamily: "HelveticaNeue-Medium", color: '#1E1611', letterSpacing: 0.4 };
+
+    if (!this.state.isReady)
+    return <AppLoading/>
+
+    return (
+      <Router>
+        <Stack key="root"
+          navigationBarStyle={{backgroundColor: "#fff"}}
+          titleStyle={navTitleStyle}
+          backButtonTintColor={'#1E1611'}>
+          <Scene key="Main" component={Main} title="Main" initial/>
+        </Stack>
+      </Router>
+    )
+  }
+}
+
+class Main extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Changes you make will automatically reload.</Text>
+      <Text>Shake your phone to open the developer menu.</Text>
+      </View>
+      );
+    }
+  }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
